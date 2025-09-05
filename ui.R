b4ff54e9ad99f863c206e14a522c72ad82c3a861
Slighting6322 <pablo.maldona@transformaciondigital.gob.mx>
2025-09-05 14:25:55 -0600

    se ha corregido el error de usar narvarPage en lugar de htmlTemplate

--- ui.R
+++ ui.R
@@ -1,19 +1,6 @@
 library(shiny)
 
-ui <- navbarPage(
-  "Tablero",
-  tabPanel(
-    "Ocupación por centro",
-    htmlTemplate(
-      "www/index.html",
-      fecha_corte_placeholder = textOutput("fecha_corte_texto", inline = TRUE)
-    )
-  ),
-  tabPanel(
-    "Origen y Destino",
-    htmlTemplate(
-      "www/origen_destino.html",
-      fecha_corte_placeholder = textOutput("fecha_corte_texto", inline = TRUE)
-    )
-  )
+ui <- htmlTemplate(
+  "www/index.html",
+  fecha_corte_placeholder = textOutput("fecha_corte_texto", inline = TRUE)
 )

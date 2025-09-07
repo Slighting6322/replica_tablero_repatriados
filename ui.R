5e5d4d1ba9dd026339f7410b896ae933578a844c
Slighting6322 <pablo.maldona@transformaciondigital.gob.mx>
2025-09-07 11:50:23 -0600

    se ha corregido la posicion de la imagen del footer

--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ ui <- fluidPage(class = "app-root-full",
     # Shiny dependencies are added by fluidPage; include fonts and CSS
     tags$link(href = "https://fonts.googleapis.com/css2?family=Inter&family=Montserrat&family=Noto+Sans&display=swap", rel = "stylesheet"),
   # Forzar recarga del CSS cuando se hacen cambios: se añade query string de versión
-  tags$link(rel = "stylesheet", href = "css/style.css?v=2"),
+  tags$link(rel = "stylesheet", href = "css/style.css?v=3"),
     tags$title("Tablero")
   ),
 
